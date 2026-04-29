02067545a733da54902f1a3ed388aee7f3f64bba
Miku AuahDark <lumi.hotaru@gmail.com>
2026-04-29 10:30:22 +0800

    Move tutorial display to separate file.

--- src/scenes/main_scene/main_scene.lua
+++ src/scenes/main_scene/main_scene.lua
@@ -1,80 +1,6 @@
 local FreeCameraScene = require("src.scenes.FreeCameraScene")
 local World = require("src.world.world")
-
-
----@param safeArea kirigami.Region
-local function renderTutorial0(safeArea)
-    local skipped = false
-    local textF = g.getMainFont(12)
-    local bs = g.getItemInfo("basic_server", "server")
-
-    local builder = ui.TooltipBuilder(safeArea.x + safeArea.w, safeArea.y + safeArea.h, 1, 1, safeArea, 150)
-    builder:addText(TEXT.TUTORIAL_0_1, textF, "center")
-    builder:addText(TEXT.TUTORIAL_0_2({server = bs.name}), textF, "center")
-
-    builder:addPadding(4)
-    builder:addCustom(32, function(x, y, w, h)
-        local r = Kirigami(x, y, w, h):padRatio(0.25, 0.25, 0.25, 0.25)
-        if ui.Button2(TEXT.TUTORIAL_SKIP, textF, objects.Color.BLACK, r) then
-            skipped = true
-        end
-    end)
-    builder:addPadding(4)
-
-    builder:render()
-
-    return skipped
-end
-
----@param safeArea kirigami.Region
-local function renderTutorial1(safeArea)
-    local skipped = false
-    local textF = g.getMainFont(12)
-    local bdi = g.getItemInfo("basic_indata", "indata")
-
-    local builder = ui.TooltipBuilder(safeArea.x + safeArea.w, safeArea.y + safeArea.h, 1, 1, safeArea, 180)
-    builder:addText(TEXT.TUTORIAL_1_1, textF, "center")
-    builder:addText(TEXT.TUTORIAL_1_2(TEXT), textF, "center")
-    builder:addText(TEXT.TUTORIAL_1_3({di = bdi.name}), textF, "center")
-
-    builder:addPadding(4)
-    builder:addCustom(32, function(x, y, w, h)
-        local r = Kirigami(x, y, w, h):padRatio(0.25, 0.25, 0.25, 0.25)
-        if ui.Button2(TEXT.TUTORIAL_SKIP, textF, objects.Color.BLACK, r) then
-            skipped = true
-        end
-    end)
-    builder:addPadding(4)
-
-    builder:render()
-
-    return skipped
-end
-
----@param safeArea kirigami.Region
-local function renderTutorial2(safeArea)
-    local skipped = false
-    local textF = g.getMainFont(12)
-    local bdo = g.getItemInfo("basic_data", "data")
-
-    local builder = ui.TooltipBuilder(safeArea.x + safeArea.w, safeArea.y + safeArea.h, 1, 1, safeArea, 150)
-    builder:addText(TEXT.TUTORIAL_2_1, textF, "center")
-    builder:addText(TEXT.TUTORIAL_2_2({["do"] = bdo.name}), textF, "center")
-
-    builder:addPadding(4)
-    builder:addCustom(32, function(x, y, w, h)
-        local r = Kirigami(x, y, w, h):padRatio(0.25, 0.25, 0.25, 0.25)
-        if ui.Button2(TEXT.TUTORIAL_SKIP, textF, objects.Color.BLACK, r) then
-            skipped = true
-        end
-    end)
-    builder:addPadding(4)
-
-    builder:render()
-
-    return skipped
-end
-
+local tutorial = require(".tutorial")
 
 ---@class MainScene: FreeCameraScene
 local MainScene = FreeCameraScene()
@@ -318,11 +244,11 @@ function MainScene:draw()
         end
 
         -- Tutorial check
-        if s.showTutorials.start == 0 and renderTutorial0(safeArea) then
+        if s.showTutorials.start == 0 and tutorial[0](safeArea) then
             s.showTutorials.start = 1
-        elseif s.showTutorials.start == 1 and renderTutorial1(safeArea) then
+        elseif s.showTutorials.start == 1 and tutorial[1](safeArea) then
             s.showTutorials.start = 2
-        elseif s.showTutorials.start == 2 and renderTutorial2(safeArea) then
+        elseif s.showTutorials.start == 2 and tutorial[2](safeArea) then
             s.showTutorials.start = 3
         end
     end
---
+++ src/scenes/main_scene/tutorial.lua
@@ -0,0 +1,79 @@
+
+---@param safeArea kirigami.Region
+local function renderTutorial0(safeArea)
+    local skipped = false
+    local textF = g.getMainFont(12)
+    local bs = g.getItemInfo("basic_server", "server")
+
+    local builder = ui.TooltipBuilder(safeArea.x + safeArea.w, safeArea.y + safeArea.h, 1, 1, safeArea, 150)
+    builder:addText(TEXT.TUTORIAL_0_1, textF, "center")
+    builder:addText(TEXT.TUTORIAL_0_2({server = bs.name}), textF, "center")
+
+    builder:addPadding(4)
+    builder:addCustom(32, function(x, y, w, h)
+        local r = Kirigami(x, y, w, h):padRatio(0.25, 0.25, 0.25, 0.25)
+        if ui.Button2(TEXT.TUTORIAL_SKIP, textF, objects.Color.BLACK, r) then
+            skipped = true
+        end
+    end)
+    builder:addPadding(4)
+
+    builder:render()
+
+    return skipped
+end
+
+---@param safeArea kirigami.Region
+local function renderTutorial1(safeArea)
+    local skipped = false
+    local textF = g.getMainFont(12)
+    local bdi = g.getItemInfo("basic_indata", "indata")
+
+    local builder = ui.TooltipBuilder(safeArea.x + safeArea.w, safeArea.y + safeArea.h, 1, 1, safeArea, 180)
+    builder:addText(TEXT.TUTORIAL_1_1, textF, "center")
+    builder:addText(TEXT.TUTORIAL_1_2(TEXT), textF, "center")
+    builder:addText(TEXT.TUTORIAL_1_3({di = bdi.name}), textF, "center")
+
+    builder:addPadding(4)
+    builder:addCustom(32, function(x, y, w, h)
+        local r = Kirigami(x, y, w, h):padRatio(0.25, 0.25, 0.25, 0.25)
+        if ui.Button2(TEXT.TUTORIAL_SKIP, textF, objects.Color.BLACK, r) then
+            skipped = true
+        end
+    end)
+    builder:addPadding(4)
+
+    builder:render()
+
+    return skipped
+end
+
+---@param safeArea kirigami.Region
+local function renderTutorial2(safeArea)
+    local skipped = false
+    local textF = g.getMainFont(12)
+    local bdo = g.getItemInfo("basic_data", "data")
+
+    local builder = ui.TooltipBuilder(safeArea.x + safeArea.w, safeArea.y + safeArea.h, 1, 1, safeArea, 150)
+    builder:addText(TEXT.TUTORIAL_2_1, textF, "center")
+    builder:addText(TEXT.TUTORIAL_2_2({["do"] = bdo.name}), textF, "center")
+
+    builder:addPadding(4)
+    builder:addCustom(32, function(x, y, w, h)
+        local r = Kirigami(x, y, w, h):padRatio(0.25, 0.25, 0.25, 0.25)
+        if ui.Button2(TEXT.TUTORIAL_SKIP, textF, objects.Color.BLACK, r) then
+            skipped = true
+        end
+    end)
+    builder:addPadding(4)
+
+    builder:render()
+
+    return skipped
+end
+
+return {
+    [0] = renderTutorial0,
+    renderTutorial1,
+    renderTutorial2
+}
